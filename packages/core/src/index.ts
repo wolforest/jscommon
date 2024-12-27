eb3878398e591c79ec8533afb153c1d12a5ef44e
tiejia <max.nie@iswallet.me>
2024-12-28 00:25:23 +0800

    feat: remove core

--- packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lodash';
-export * from './dayjs';
-export * from './hooks';
-export * from './ramda';
-export * from './request';
-export * from './vueuse';
-export * from './utils';
--- packages/core/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export { default as qs } from 'qs';
-export { default as queryString } from 'query-string';
-export { pinyin } from 'pinyin-pro';
-export { default as Big } from 'big.js';
-export { default as classNames } from 'classnames'; 
